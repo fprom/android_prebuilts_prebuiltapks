603e5bd66a8fff36c3194d0a02e5104a50144feb
FPRom Builder <FPRom@example.com>
2020-07-05 16:32:19 +0100

    Move back to other Aurora Services version as that one not working

--- AuroraServices/Android.mk
+++ AuroraServices/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := privapp-permissions-com.aurora.services.xml
+LOCAL_MODULE := privapp-permissions-auroraservices.xml
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
@@ -16,5 +16,6 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_REQUIRED_MODULES := privapp-permissions-com.aurora.services.xml
+LOCAL_REQUIRED_MODULES := privapp-permissions-auroraservices.xml
 include $(BUILD_PREBUILT)
+
