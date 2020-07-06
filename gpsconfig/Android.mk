fff60e78697b8e65b8962e07dacb5814cc96ee1e
FPRom Builder <FPRom@example.com>
2020-07-06 12:02:11 +0100

    Doubt this will work...

--- gpsconfig/Android.mk
+++ gpsconfig/Android.mk
@@ -8,3 +8,10 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
 LOCAL_SRC_FILES := gps_psds.conf
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := gps_debug.conf
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
+LOCAL_SRC_FILES := gps_psds.conf
+include $(BUILD_PREBUILT)
